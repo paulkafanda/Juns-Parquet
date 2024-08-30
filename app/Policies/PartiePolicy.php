9c0c1714ccda14ab8060030abc6b7faefd31e1fb
Jean luc <20ka208@esisalama.org>
2024-08-30 10:39:24 +0100

    Merge remote-tracking branch 'origin/features/roles-permissions' into features/roles-permissions

---
+++ app/Policies/PartiePolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class PartiePolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
