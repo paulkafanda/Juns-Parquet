17df5b80179f2ef3683d5b56b09f2b4490114708
kafandapaul <paulkaf84@gmail.com>
2024-08-30 19:07:29 +0200

    --hot-fix refactor user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Enums\UserRole;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -70,5 +73,32 @@ class User extends Authenticatable
     public function isJuge(): bool
     {
         return $this->role === UserRole::JUGE;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getForm(): array
+    {
+        return [
+            TextInput::make('name')
+                ->required(),
+            TextInput::make('email')
+                ->email()
+                ->required(),
+            TextInput::make('post_nom'),
+            TextInput::make('prenom'),
+            TextInput::make('tel')
+                ->tel(),
+            TextInput::make('sexe'),
+            TextInput::make('adresse'),
+            Select::make('role')
+                ->enum(UserRole::class)
+                ->options(UserRole::class),
+            DateTimePicker::make('email_verified_at'),
+            TextInput::make('password')
+                ->password()
+                ->required(),
+        ];
     }
 }
