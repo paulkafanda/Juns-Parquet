8ed4899256625eab37708d7f81e780795ee346a0
kafandapaul <paulkaf84@gmail.com>
2024-09-01 00:50:49 +0200

    --fix create action label

--- app/Filament/Resources/DossierResource/Pages/ListDossiers.php
+++ app/Filament/Resources/DossierResource/Pages/ListDossiers.php
@@ -13,7 +13,8 @@ class ListDossiers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Creer un  nouveau dossier'),
         ];
     }
 }
