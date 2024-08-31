c1472ec9a13e126bb1dac5baf33089f4152e4db8
kafandapaul <paulkaf84@gmail.com>
2024-08-31 23:53:12 +0200

    --fix gate

--- app/Filament/Clusters/DossierCluster.php
+++ app/Filament/Clusters/DossierCluster.php
@@ -7,4 +7,9 @@ use Filament\Clusters\Cluster;
 class DossierCluster extends Cluster
 {
     protected static ?string $navigationIcon = 'heroicon-o-folder';
+
+    public static function canAccess(): bool
+    {
+        return ! (auth()->user()->isSecretaire() || auth()->user()->isChefOffice());
+    }
 }
